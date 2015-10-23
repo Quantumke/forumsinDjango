509e83cbf9723dd7a8adf699a01d8da16ae4c4f5
Benson Mburu <nguruben@gmail.com>
2015-10-23 07:08:16 +0300

    added post views

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,9 @@ def view_post(request, slug):
     return render_to_response('view_post.html', {
         'post': get_object_or_404(Blog, slug=slug)
     })
+    def view_category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    return render_to_response('view_category.html', {
+        'category': category,
+        'posts': Blog.objects.filter(category=category)[:5]
+    })
