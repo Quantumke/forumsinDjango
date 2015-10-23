b8eaeed179764e649bf96b42203a2ec9daab49c3
Benson Mburu <nguruben@gmail.com>
2015-10-23 06:55:17 +0300

    defined fields in model

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    body = models.TextField()
+    posted = models.DateField(db_index=True, auto_now_add=True)
+    category = models.ForeignKey('blog.Category')
