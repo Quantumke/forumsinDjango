4400d4874a6ea601b7238e73a06a420a9de85a3c
Benson Mburu <nguruben@gmail.com>
2015-10-23 07:05:05 +0300

    added home view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from djangorocks.blog.models import Blog, Category
+from django.shortcuts import render_to_response, get_object_or_404
+
 
 # Create your views here.
+def index(request):
+    return render_to_response('index.html', {
+        'categories': Category.objects.all(),
+        'posts': Blog.objects.all()[:5]
+    })
